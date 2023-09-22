85f5d43f1404d23bdc6439cc25c178e94be42d9f
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-09-21 20:55:55 -0400

    added movement scripts, added up to vishnu

--- My project/Assets/Scripts/CubeRotation.cs
+++ My project/Assets/Scripts/CubeRotation.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class CubeRotation : MonoBehaviour
 {
 
-    public float rotationSpeed = 45f;
+    //public float rotationSpeed = 45f;
+
+    public float xAngle, yAngle, zAngle;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,7 @@ public class CubeRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        //transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
     }
 }
