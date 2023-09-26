300e7f1810bbb5cb5584966ba34978e2b1a2a36c
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-09-25 21:13:15 -0400

    fernande

--- My project/Assets/Scripts/FadeGameObject.cs
+++ My project/Assets/Scripts/FadeGameObject.cs
@@ -4,95 +4,47 @@ using UnityEngine;
 
 public class FadeGameObject : MonoBehaviour
 {
-    
-    private Renderer _objectRender;
-    private Color _originalColor;
-    /*private Color _targetColor;
-    private float fadeDuration = 1f;
-    private float currentTime = 0f;
-    private bool isFading = false;*/
+    private GameObject[] _objectsToCycle;
+    private int currentIndex = 0;
+    private int numObjects;
     
     // Start is called before the first frame update
     void Start()
     {
-        //get the renderer component of the gameObject
-        _objectRender = GetComponent<Renderer>();
+        _objectsToCycle = GameObject.FindGameObjectsWithTag("ObjectsToCycle");
+        numObjects = _objectsToCycle.Length;
 
-        //store original color including alpha
-        _originalColor = _objectRender.material.color;
-        
-        //set the target color with the same
-        //RGB values as the original but set alpha to zero
-        //_targetColor = new Color(_originalColor.r,
-           //_originalColor.g, _originalColor.b, 0);
+        for (int i = 1; i < numObjects; i++)
+        {
+            SetAlpha(_objectsToCycle[i], 0f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //check if the obj is currently fading
-        /*if (isFading)
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            currentTime += Time.deltaTime;
+            SetAlpha(_objectsToCycle[currentIndex], 0f);
+            //Debug.Log("TransparentSpriteNo:" +currentIndex);
+            currentIndex = (currentIndex + 1) % numObjects;
+            SetAlpha(_objectsToCycle[currentIndex], 1f);
+            //Debug.Log("OpaqueSpriteNo:" +currentIndex);
+        }
+    }
 
-            //clamp01 clamps value between 1 n 0
-            //calculate the lerp factor
-            float lerpFactor = Mathf.Clamp01(currentTime / fadeDuration);
-
-            //set lerped value to a value in between 1 and 0 over lerpedFactor (time)
-            Color lerpedColor = Color.Lerp(_originalColor, _targetColor,
-                lerpFactor);
-            
-            //the renderer material color equals the lerped color
-            _objectRender.material.color = lerpedColor;
-
-            //if the lerp factor (the time it takes) is less than 1f
-            //than is fading is false
-            //checks if the fade is complete
-            if (lerpFactor >= 1f)
+    private void SetAlpha(GameObject parent, float alpha)
+    {
+        SpriteRenderer[] spriteRenderers = parent.GetComponentsInChildren<SpriteRenderer>();
+        
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers)
             {
-                isFading = false;
+                Color color = spriteRenderer.color;
+                color.a = alpha;
+                spriteRenderer.color = color;
             }
-        }*/
-    }
-
-    public void FadeIn(float duration)
-    {
-        Color newColor = _originalColor;
-        newColor.a = 1f;
-        _objectRender.material.color = newColor;
         
-        //set fade duration
-        //this gets passed into the function
-        //fadeDuration = duration;
-        //Debug.Log("Fade Duration:" + fadeDuration);
-
-        //reset the current time
-        //currentTime = 0f;
-        //Debug.Log("Current Time " + currentTime);
-
-        //start fading
-        //isFading = true;
-        //Debug.Log("isFading " + isFading);
+        Debug.Log("Alpha of " + parent.name + "is now" + alpha);
     }
-
-    public void FadeOut(float duration)
-    {
-
-        Color newColor = _originalColor;
-        newColor.a = 0f;
-        _objectRender.material.color = newColor;
-        
-        //set fade duration
-        //fadeDuration = duration;
-        //Debug.Log("Fade Duration:" + fadeDuration);
-
-        //reset current time
-        //currentTime = 0f;
-        //Debug.Log("Current Time " + currentTime);
-
-        //stop fading
-        //isFading = true;
-        //Debug.Log("isFading " + isFading);
-    }
+    
 }
---
+++ My project/Assets/Scripts/NextSprite.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextSprite : MonoBehaviour
+{
+    private SpriteRenderer[] _sprites; //array to store the sprites
+    private int currentIndex = 0; //current index of sprites set at zero
+    private int numSprites; //the number of sprites
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get children on game object sprite renderers and
+        //store it in sprites which is an array
+        _sprites = GetComponentsInChildren<SpriteRenderer>();
+        //the length of the sprites store that number into the amount of sprites
+        numSprites = _sprites.Length;
+
+        //hide all the sprites but the first sprite
+        //set their alpha to zero
+        //i euqal's 1, and 1 is less than the total number of
+        //sprites(9) than go to the next part of the list
+        for (int i = 1; i < numSprites; i++)
+        {
+            //all renderers of children, their color equals
+            // new color where the alpha is set to zero
+            _sprites[i].color = new Color(1f, 1f, 1f, 0f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if you click the right arrow
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //get the array of sprite renderes and the number on the list and their color
+            //set to new color where alpha is zero
+            Debug.Log("key pressed");
+            _sprites[currentIndex].color = new Color(1f, 1f, 1f, 0f);
+            Debug.Log("set transparent");
+
+            //storing int variable = (0 + 1)
+            //increments current index by adding 1, wraps around
+            currentIndex = (currentIndex + 1) % numSprites;
+
+            //get the array of sprite renderes and the number on the list and their color
+            //set to new color where alpha is one
+            _sprites[currentIndex].color = new Color(1f, 1f, 1f, 1f);
+            Debug.Log("set opaque");
+        }
+    }
+
+    void FadeIn()
+    {
+       
+
+    }
+
+    void FadeOut()
+    {
+        
+    }
+    void SetFade()
+    {
+        
+    }
+}
