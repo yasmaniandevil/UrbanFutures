22f2bf811c91e1a7fe460a950fe40d15ca0eef93
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-09-26 21:36:02 -0400

    fernande basically done

--- My project/Assets/Scripts/FadeGameObject.cs
+++ My project/Assets/Scripts/FadeGameObject.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class FadeGameObject : MonoBehaviour
 {
+    //array to store objects to cycle
     private GameObject[] _objectsToCycle;
+    //index of currently visable obj
     private int currentIndex = 0;
+    //number of objects
     private int numObjects;
     
     // Start is called before the first frame update
     void Start()
     {
+        //find gameobj with the tags and store it in objectsToCycle
         _objectsToCycle = GameObject.FindGameObjectsWithTag("ObjectsToCycle");
+        //count the length of the objs to cycle legth store in num objects
         numObjects = _objectsToCycle.Length;
 
+        //Set the alpha of all objects except the first one to 0, making them transparent
         for (int i = 1; i < numObjects; i++)
         {
+            //
             SetAlpha(_objectsToCycle[i], 0f);
         }
     }
@@ -23,25 +30,45 @@ public class FadeGameObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        //if e is pressed
+        if (Input.GetKeyDown(KeyCode.E))
         {
+            
+            //set alpha of currently visible obj to 0
             SetAlpha(_objectsToCycle[currentIndex], 0f);
-            Debug.Log("TransparentSpriteNo:" +currentIndex);
+            //Debug.Log("TransparentSpriteNo:" +currentIndex);
+            //index goes up by one
             currentIndex = (currentIndex + 1) % numObjects;
             Debug.Log("current index:" + currentIndex);
+            //set alpha of next obj in array to 1
             SetAlpha(_objectsToCycle[currentIndex], 1f);
-            Debug.Log("OpaqueSpriteNo:" +currentIndex);
+            //Debug.Log("Sprite ON:" +currentIndex);
         }
     }
 
     private void SetAlpha(GameObject parent, float alpha)
     {
+        //get all mesh renderers in the parent obj and its children
         MeshRenderer[] meshRender = parent.GetComponentsInChildren<MeshRenderer>();
         
+        
+        /*It declares a variable meshRenderer of type MeshRenderer that represents
+         each element in the meshRender array as it iterates through it.
+         Inside the loop, you can access and manipulate each MeshRenderer component individually.*/
+        
+        //loop through each mesh renderer
             foreach (MeshRenderer meshRenderer in meshRender)
             {
+                
+                //access to individual component on the renderer stored in the meshRender variable
+                //access then modify the properties or fields of the component
+                //get the current color and update it alpha component
+                
+                //retrives current color of material
                 Color color = meshRenderer.material.color;
+                //updates in alpha
                 color.a = alpha;
+                //assigns modified color back
                 meshRenderer.material.color = color;
             }
 
