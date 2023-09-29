1e8c0d5d0d2f86b7245320eb68fc19f6662f8c3f
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-09-29 13:41:33 -0400

    liad audio done

--- My project/Assets/Scripts/AudioScript.cs
+++ My project/Assets/Scripts/AudioScript.cs
@@ -1,16 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using TMPro;
 
 public class AudioScript : MonoBehaviour
 {
     public AudioSource audioSource;
+    public Button button;
+    private bool playerEntered = false;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+
+        //GameObject.FindWithTag("Player");
+
+
     }
-    public static class FadeAudioSource
+    /*public static class FadeAudioSource
     {
         public static IEnumerator startFade(AudioSource audioSource, float duration, float targetVolume)
         {
@@ -25,12 +34,66 @@ public class AudioScript : MonoBehaviour
             
             yield break;
         }
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(FadeAudioSource.startFade(audioSource, 100, .1f));
+        //StartCoroutine(FadeAudioSource.startFade(audioSource, 100, .1f));
         //Debug.Log("current volume " + audioSource.volume);
+        
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Debug.Log("Q clicked");
+            audioSource.Pause();
+            Debug.Log("audio paused");
+        }
+        
+        
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerEntered = true; // Player has entered the trigger area
+            PlayAudioIfPlayerEntered();
+            Debug.Log("player entered set to true");
+            Debug.Log("Player entered the trigger area");
+        }
+    }
+    
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerEntered = false; // Player has exited the trigger area
+            audioSource.Pause();
+            Debug.Log("player entered set to false");
+            Debug.Log("Player exited the trigger area");
+        }
+    }
+
+    /*public void PauseButton()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Pause();
+            Debug.Log("Liad audio paused");
+        }
+    }*/
+    
+    void PlayAudioIfPlayerEntered()
+    {
+        if (playerEntered == true)
+        {
+            Debug.Log("PlauAudioEntered");
+            audioSource.Play();
+            Debug.Log("Audio source" +audioSource.name);
+            Debug.Log("Audio played because player is in the trigger area");
+        }
+    }
+    
+    
+    
 }
